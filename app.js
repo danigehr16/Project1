b5244b23f723b07ebe35acb985b564ca11711577
Jay Rodriguez <jared.m.rodriguez@gmail.com>
2020-03-30 15:02:10 -0500

    updates to include appropriate firebase credentials & simplification of js template for our purposes

--- app.js
+++ app.js
@@ -1,83 +1,61 @@
 // 1. Initialize Firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyDGHSJCWI0-CpE8J-3Qdpxr-D9GiCDQZyc",
-    authDomain: "train-scheduler-f1866.firebaseapp.com",
-    databaseURL: "https://train-scheduler-f1866.firebaseio.com",
-    projectId: "train-scheduler-f1866",
-    storageBucket: "train-scheduler-f1866.appspot.com",
-    messagingSenderId: "270883119658",
-    appId: "1:270883119658:web:2c397d9201aa55b85b1953"
-};
+var firebaseConfig = {
+    apiKey: "AIzaSyBYXHZ_u0F08l7IcwzHpf5unJe_4tYa_Wo",
+    authDomain: "tp-search.firebaseapp.com",
+    databaseURL: "https://tp-search.firebaseio.com",
+    projectId: "tp-search",
+    storageBucket: "tp-search.appspot.com",
+    messagingSenderId: "922115969314",
+    appId: "1:922115969314:web:996eff6737bb582db1557b"
+  };
 
 firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 
-// 2. Button for adding Trains
+// 2. Button for adding tp searches
 $("#add-tp-btn").on("click", function (event) {
     event.preventDefault();
 
     // Grabs user input
-    var trnName = $("#train-name-input").val().trim();
-    var trnDestination = $("#destination-input").val().trim();
-    var trnStart = $("#start-input").val().trim();
-    var trnFrequency = $("#frequency-input").val().trim();
+    var tpstoreName = $("#store-name-input").val().trim();
+    var tpaddress = $("#address-input").val().trim();
+    var tpsku = $("#sku-input").val().trim();
 
-    // Creates local "temporary" object for holding train data
-    var newTrain = {
-        name: trnName,
-        destination: trnDestination,
-        start: trnStart,
-        frequency: trnFrequency,
+    // Creates local "temporary" object for holding tp data
+    var newTP = {
+        name: tpstoreName,
+        address: tpaddress,
+        sku: tpsku,
     };
 
-    // Uploads train data to the database
-    database.ref().push(newTrain);
+    // Uploads tp data to the database
+    database.ref().push(newTP);
 
-    alert("Train successfully added");
+    alert("TP successfully added");
 
     // Clears all of the text-boxes
-    $("#train-name-input").val("");
-    $("#destination-input").val("");
-    $("#start-input").val("");
-    $("#frequency-input").val("");
+    $("#store-name-input").val("");
+    $("#address-input").val("");
+    $("#sku-input").val("");
 });
 
-// 3. Create Firebase event for adding Train to the database and a row in the html when a user adds an entry
+// 3. Create Firebase event for adding TP to the database and a row in the html when a user adds an entry
 database.ref().on("child_added", function (childSnapshot) {
     console.log(childSnapshot.val());
 
     // Store everything into a variable.
-    var trnName = childSnapshot.val().name;
-    var trnDestination = childSnapshot.val().destination;
-    var trnStart = childSnapshot.val().start;
-    var trnFrequency = childSnapshot.val().frequency;
+    var tpstoreName = childSnapshot.val().name;
+    var tpaddress = childSnapshot.val().address;
+    var tpsku = childSnapshot.val().sku;
 
-     // First Time (pushed back 1 year to make sure it comes before current time)
-     var firstTimeConverted = moment(trnStart, "HH:mm").subtract(1, "years");
-     console.log("First Time Converted: " + firstTimeConverted);
- 
-     var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-     var tRemainder = diffTime % trnFrequency;
- 
-     // Minute Until Train
-     var tMinutesTillTrain = trnFrequency - tRemainder;
-     console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
- 
-     // Next Train
-     var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-     moment(nextTrain).format("HH:mm");
-     console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm"));
-    
     // Create the new row
     var newRow = $("<tr>").append(
-        $("<td>").text(trnName),
-        $("<td>").text(trnDestination),
-        $("<td>").text(trnFrequency),
-        $("<td>").text(nextTrain),
-        $("<td>").text(tMinutesTillTrain)
+        $("<td>").text(tpstoreName),
+        $("<td>").text(tpaddress),
+        $("<td>").text(tpsku),
     );
 
     // Append the new row to the table
-    $("#train-table > tbody").append(newRow);
+    $("#tp-table > tbody").append(newRow);
 });
