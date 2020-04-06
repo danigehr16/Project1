82eedd4b6a545633f47cb06a5433ad473a2385e6
ThePalad1n <evangreen91@gmail.com>
2020-04-06 13:50:46 -0400

    big changes

--- assets/javascript/mapScript.js
+++ assets/javascript/mapScript.js
@@ -1,3 +1,17 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBYXHZ_u0F08l7IcwzHpf5unJe_4tYa_Wo",
+  authDomain: "tp-search.firebaseapp.com",
+  databaseURL: "https://tp-search.firebaseio.com",
+  projectId: "tp-search",
+  storageBucket: "tp-search.appspot.com",
+  messagingSenderId: "922115969314",
+  appId: "1:922115969314:web:996eff6737bb582db1557b"
+};
+
+
+
+
+
 /**
  * A full list of available request parameters can be found in the Geocoder API documentation.
  * see: http://developer.here.com/rest-apis/documentation/geocoder/topics/resource-geocode.html
